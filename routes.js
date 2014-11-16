75f33295844ee8de42ac41f3790e9826560c45ab
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-11-16 13:08:46 -0500

    removed console mg

--- routes.js
+++ routes.js
@@ -1,20 +1,21 @@
 'use strict';
-var path = require('path');
+// var path = require('path');
 
 module.exports = function (periodic) {
-	// console.log('periodic.app.locals', periodic.app.locals);
 	var readerthemeController = require('./controller/readertheme')(periodic),
-		itemController = require(path.join(process.cwd(), 'app/controller/item'))(periodic),
-		themeRouter = periodic.express.Router();
+		themeRouter = periodic.express.Router(),
+		envThemeSettings = periodic.settings.themeSettings.settings[periodic.settings.application.environment];
 
-	periodic.app.locals.showperiodiccredit = periodic.settings.themeSettings.settings.showperiodiccredit;
-	periodic.app.locals.showadminlink = periodic.settings.themeSettings.settings.showadminlink;
-	periodic.app.locals.navlinks = periodic.settings.themeSettings.settings.navlinks;
-	periodic.app.locals.autohidetitle = periodic.settings.themeSettings.settings.autohidetitle;
-	periodic.app.locals.usecustomnav = periodic.settings.themeSettings.settings.usecustomnav;
-	periodic.app.locals.customnav = periodic.settings.themeSettings.settings.customnav;
+	if (periodic.settings.themeSettings) {
+		periodic.app.locals.showperiodiccredit = envThemeSettings.showperiodiccredit;
+		periodic.app.locals.showadminlink = envThemeSettings.showadminlink;
+		periodic.app.locals.navlinks = envThemeSettings.navlinks;
+		periodic.app.locals.autohidetitle = envThemeSettings.autohidetitle;
+		periodic.app.locals.usecustomnav = envThemeSettings.usecustomnav;
+		periodic.app.locals.customnav = envThemeSettings.customnav;
+	}
 
-	if (periodic.settings.themeSettings.settings.navlinks === 'categories') {
+	if (envThemeSettings.navlinks === 'categories') {
 		themeRouter.get('/browse/:entitytype|/browse/:entitytype/:entityitems|/author/:id|/search|/404|/notfound|/collections|/collection/:id|/collection/search|/items|/item/search|/articles|/item/:id|/', readerthemeController.loadNavCategories, readerthemeController.getNavCategories);
 	}
 
